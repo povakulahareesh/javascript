a9a64610ca4098bd3ffaf376b8ed104a9c113516
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 23:06:53 +0530

    l

---
+++ three.js
@@ -0,0 +1,42 @@
+let a=[10,20,"sai"]
+let user={}
+let account={
+    acc_id:101,
+    acc_name:"hareesh"
+} 
+
+
+Object.keys(user).length>0 ? console.log("not empty object"): console.log("it is empty object")
+
+//console.log(a.length>7)
+
+//console.log(Object.keys(user).length>0)
+
+
+
+
+
+
+
+//console.log(account.length)
+//console.log(Object.keys(account).length>0)
+
+
+//console.log (Object.keys(user).length>0)
+//console.log(a.length)
+//console.log (Object.keys(account).length>0)
+//console.log (Object.keys(account))
+//console.log (Object.keys(account).length)
+// console.log(Object.keys(user))
+//  console.log(Object.keys(account).length)
+//  console.log(Object.keys(account).length>0)
+
+ 
+ //Object.keys(user).length>0 ? console.log("not empty pbject"): console.log("empty object")
+
+// if(Object.keys(user).length>0){
+//     console.log("not empty set")
+// }
+// else{
+//     console.log("empty object")
+// }
