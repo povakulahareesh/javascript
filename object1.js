c4f061634f2335096bad80aa35139bbd00baea30
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 11:41:12 +0530

    js

---
+++ object1.js
@@ -0,0 +1,42 @@
+
+//knowing object properties
+
+
+let emp={
+    eid:104,
+    name:"hareesh",
+    sal:45000,
+ }//no indexing//it is not itarable
+
+ 
+ for( prop in emp){
+     console.log(prop)
+ }
+
+
+
+
+//for knowing values
+
+ //let emp={
+    //eid:104,
+   //  name:"hareesh",
+  //  sal:45000,
+//}
+// //console.log(emp.eid) //this is one method
+
+
+
+// //knowing object how much properties have
+
+//  for( keys in emp){    
+// //  console.log(emp.prop)// undefined
+//  // console.log("ge") //it is optional
+
+//  //know object values
+//  console.log(emp[keys])
+//  }
+
+//  //next class symbol,,operators,,conditional fow statements//functions
+
+
