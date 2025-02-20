ea24b60d9e526d484d089f44e8abce591c051e2b
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 22:10:35 +0530

    javs script

---
+++ arrow.js
@@ -0,0 +1,16 @@
+
+  product =(num1,num2)=> {
+    var result =num1*num2
+    return result
+  }
+  console.log(product(7,56))
+
+  
+  product =(num1,num2)=> {
+    var result =num1*num2
+console.log(result)
+  }
+  (product(7,56))
+
+
+
