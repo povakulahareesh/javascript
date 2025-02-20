443078ab0a1e0254a0aa63875c7e789cda1a0fa8
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 11:29:57 +0530

    hlooo

---
+++ iterate.js
@@ -0,0 +1,12 @@
+let names=["Rahul","siva","Arjum","kumar"]
+
+
+let arr=[]
+ for(let name of names){
+  console.log(name[0]);
+ }
+
+ for(let name of names){
+    arr.push(name[0])
+ }
+console.log(arr)
--- one1.js
+++ one1.js
@@ -1,5 +1,10 @@
-var i=2;
-while(i<=10){
-    console.log(i);
-    i++;
+let a={}
+let b={"id":101}
+let emp={ 
+    eid:101,
+    ename:"rahul",
+    esal:4500.45
 }
+console.log(a)
+console.log(b)
+console.log(emp)
