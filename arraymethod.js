905e3b6a4044765abf4832ee464f49185b71da7c
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 22:27:55 +0530

    methods

---
+++ arraymethod.js
@@ -0,0 +1,101 @@
+
+//  //unshift methods
+ var movies=['aa','ah','ii'] 
+ movies .shift()
+  console.log(movies)
+
+
+
+
+//  //push method
+  var movies=['aa','ah','ii'] 
+movies .push("33")
+ console.log(movies)
+
+
+
+// //shift method
+ 
+// var movies=['aa','ah','ii'] 
+// movies .shift()
+// console.log(movies)
+
+//remove element at end of the array
+// 
+// // 
+// var movies=['aa','ah','ii'] 
+// movies .pop()
+// console.log(movies)
+
+
+
+// // slice
+// var movies=['aa','ah','ii'] 
+
+// console.log (movies)
+
+// console.log(movies.slice(0,2))
+
+
+
+
+
+
+///concatination
+ //let movies=['aa','ah','ii','ll','oo'] 
+ //let names=movies.concat('haresh')
+//console.log(names)
+//console.log(movies)
+
+
+
+
+
+
+
+// // indexing
+// let movies=['aa','ah','ii','ll','oo'] 
+// console.log(movies.indexOf("ll"))
+
+
+
+
+
+//  //indexing  //knowing element index
+// let movies=['aa','ah','ii','ll','oo'] 
+// console.log(movies.indexOf("ll"))
+
+
+
+//reverse method
+
+
+// let movies=['aa','ah','ii','ll','oo'] 
+// movies.reverse()
+// console.log(movies)
+
+
+
+
+// let movies=['aa','ah','ii','ll','oo'] 
+// movies.reverse()
+// console.log(movies.indexOf("ii"))
+//if specified element not available it return -1
+
+
+
+// let movies=['aa','ah','ii','ll','oo'] 
+// movies.reverse()
+// console.log(movies.indexOf("77"))
+
+//js array es6 methods
+//eids push
+//each()
+//filter
+//reduce
+//call
+//apply
+
+
+
+
