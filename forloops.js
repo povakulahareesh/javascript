886643a1e9e8b7f389564cca57ee318f1abd19c5
povakulahareesh <hareeshpovakula@gmail.com>
2025-02-20 22:56:10 +0530

    loops

---
+++ forloops.js
@@ -0,0 +1,17 @@
+let names=["Rahul","siva","Arjum","kumar"]
+
+  arr=[]
+
+// // //            0     1        2          3
+
+ for(let name of names){
+  console.log(name[0]);
+     }
+
+
+
+  for (let name of names){
+ arr.push(name[0])
+   }
+
+ console.log(arr);
