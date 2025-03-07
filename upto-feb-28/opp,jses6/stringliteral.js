1590bf9fd2a1a1b564f968329c511e5731a5eb93
povakulahareesh <hareeshpovakula@gmail.com>
2025-03-07 15:43:57 +0530

    txt

---
+++ upto-feb-28/opp,jses6/stringliteral.js
@@ -0,0 +1,15 @@
+
+//what is string literal
+//read variable in the form of string  &  ${}(`)--->backtick--->construct urls
+//where we use string literal
+// console.log("employee id :${}eid");
+
+eid=432
+ename="sivaji","swapna"
+console.log("ename:",ename,"and eid:",eid);
+
+//read variable in the  form of string
+// console.log(`empid:eid and name:ename`);
+console.log(`empid:${eid} and name:${ename}`);
+
+
